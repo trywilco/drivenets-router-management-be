b949012edf33fffad675f40465225ac96e061282
Shem <smagnezi8@gmail.com>
2025-04-20 14:02:24 +0000

    fix envs and +x

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,15 +1,16 @@
 WILCO_ID="`cat .wilco`"
-CODESPACE_BACKEND_HOST=$(curl -s "${ENGINE_BASE_URL}/api/v1/codespace/backendHost?codespaceName=${CODESPACE_NAME}&portForwarding=${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" | jq -r '.codespaceBackendHost')
-CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
+
 export ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 
 # Update engine that codespace started for user
 curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
 
-# Export backend envs when in codespaces
-echo "export CODESPACE_BACKEND_HOST=\"${CODESPACE_BACKEND_HOST}\"" >> ~/.bashrc
+gh codespace ports visibility 3000:public --codespace $CODESPACE_NAME
+gh codespace ports visibility 3001:public --codespace $CODESPACE_NAME
+
+CODESPACE_BACKEND_URL="https://${CODESPACE_NAME}-3001.app.github.dev"
 echo "export CODESPACE_BACKEND_URL=\"${CODESPACE_BACKEND_URL}\"" >> ~/.bashrc
-echo "export CODESPACE_WDS_SOCKET_PORT=443" >> ~/.bashrc
+echo "export REACT_APP_CODESPACE_BACKEND_URL=\"${CODESPACE_BACKEND_URL}\"" >> ~/.bashrc
 
 # Export welcome prompt in bash:
 echo "printf \"\n\n☁️☁️☁️️ Wilco: Develop in the Cloud ☁️☁️☁️\n\"" >> ~/.bashrc
